9f4dceabca1a93f3cdbd7ba5b30af2c1fbb6ee54
LEONID <312agapov@gmai.com>
2024-03-02 18:46:26 +0400

    first commit

---
+++ src/main/java/ru/agapov/DSP/DspApplication.java
@@ -0,0 +1,13 @@
+package ru.agapov.DSP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/agapov/DSP/DspApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.agapov.DSP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
